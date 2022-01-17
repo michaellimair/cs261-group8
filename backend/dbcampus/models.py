4be6d0498900383139d0939fc4fcc1c9a1b9e028
Michael Lim <michaellimbong@gmail.com>
2022-01-17 21:07:48 +0000

    initial project setup

---
+++ backend/dbcampus/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Todo(models.Model):
+  title = models.CharField(max_length=120)
+  subtitle = models.CharField(max_length=120)
+  description = models.TextField()
+  completed = models.BooleanField(default=False)
+
+  def _str_(self):
+    return self.title
+
+  def describe(self):
+    return self.title + ": " + self.subtitle
