f0a5b01df44bbcad73f810b6dad05c109dedc151
Michael Lim <michaellimbong@gmail.com>
2022-02-10 02:20:22 +0000

    feat: link registration screen to api

--- frontend/src/pages/register.test.tsx
+++ frontend/src/pages/register.test.tsx
@@ -1,67 +1,175 @@
-import { render } from '@testing-library/react';
+/* eslint-disable no-console */
+import {
+  act, fireEvent, render, RenderResult,
+} from '@testing-library/react';
+import { httpClient } from 'api';
+import BadRequestApiError from 'api/error/BadRequestApiError';
+import { IRegistration, IRegistrationError } from 'customTypes/auth';
+import { QueryClient, QueryClientProvider, setLogger } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
 import RegisterPage from './register';
 
+jest.mock('api');
+
+const tick = (ms: number) => new Promise((resolve) => {
+  setTimeout(resolve, ms);
+});
+
 describe('register', () => {
-  it('renders properly', () => {
-    const result = render(
-      <BrowserRouter>
-        <RegisterPage />
-      </BrowserRouter>,
+  let queryClient: QueryClient;
+  let result: RenderResult<typeof import('@testing-library/dom/types/queries'), HTMLElement>;
+  let registerButton: HTMLElement;
+
+  beforeAll(() => {
+    queryClient = new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false,
+          cacheTime: Infinity,
+        },
+      },
+    });
+
+    setLogger({
+      log: console.log,
+      warn: console.warn,
+      // ✅ no more errors on the console
+      error: () => {},
+    });
+  });
+
+  beforeEach(() => {
+    result = render(
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <RegisterPage />
+        </BrowserRouter>
+      </QueryClientProvider>,
     );
+    registerButton = result.queryByTestId('registerButton')!;
+    if (!registerButton) {
+      throw new Error('Register button does not exist!');
+    }
+  });
+
+  afterEach(() => {
+    result.unmount();
+  });
+
+  it('renders properly', () => {
     expect(result).toMatchSnapshot();
   });
 
   it('goes to login page when login button is clicked', async () => {
-    const result = render(
-      <BrowserRouter>
-        <RegisterPage />
-      </BrowserRouter>,
-    );
     const loginButton = result.queryByTestId('login-button');
     if (!loginButton) {
       throw new Error('Login button cannot be found!');
     }
-    loginButton.click();
+    act(() => {
+      loginButton.click();
+    });
     expect(window.location.pathname).toBe('/auth');
   });
 
+  const passwordHiddenId = 'password-hidden';
+  const passwordShownId = 'password-shown';
+
   it('defaults to not showing password', async () => {
-    const result = render(
-      <BrowserRouter>
-        <RegisterPage />
-      </BrowserRouter>,
-    );
-    const passwordOffIcon = result.queryByTestId('password-hidden');
+    const passwordOffIcon = result.queryByTestId(passwordHiddenId);
     expect(passwordOffIcon).not.toBeEmptyDOMElement();
-    const passwordOnIcon = result.queryByTestId('password-shown');
+    const passwordOnIcon = result.queryByTestId(passwordShownId);
     expect(passwordOnIcon).toBeNull();
   });
 
   it('shows password when the show password button is clicked', async () => {
-    const result = render(
-      <BrowserRouter>
-        <RegisterPage />
-      </BrowserRouter>,
-    );
     const showPassButton = result.queryByTestId('show-password-button');
     if (!showPassButton) {
       throw new Error('Cannot find button to show password!');
     }
-    showPassButton.click();
+    act(() => {
+      showPassButton.click();
+    });
     let passwordOffIcon: HTMLElement | null;
     let passwordOnIcon: HTMLElement | null;
 
-    passwordOffIcon = result.queryByTestId('password-hidden');
+    passwordOffIcon = result.queryByTestId(passwordHiddenId);
     expect(passwordOffIcon).toBeNull();
-    passwordOnIcon = result.queryByTestId('password-shown');
+    passwordOnIcon = result.queryByTestId(passwordShownId);
     expect(passwordOnIcon).not.toBeEmptyDOMElement();
 
-    showPassButton.click();
+    act(() => {
+      showPassButton.click();
+    });
 
-    passwordOffIcon = result.queryByTestId('password-hidden');
+    passwordOffIcon = result.queryByTestId(passwordHiddenId);
     expect(passwordOffIcon).not.toBeEmptyDOMElement();
-    passwordOnIcon = result.queryByTestId('password-shown');
+    passwordOnIcon = result.queryByTestId(passwordShownId);
     expect(passwordOnIcon).toBeNull();
+  });
+
+  const fillForm = () => {
+    const userData: IRegistration = {
+      email: 'testuser@test.com',
+      username: 'testuser',
+      password: 'testuser1',
+      verify_password: 'testuser1',
+      first_name: 'Test',
+      last_name: 'User',
+    };
+
+    Object.entries(userData).forEach(([key, value]) => {
+      const inputField = result.queryByTestId(key);
+      if (!inputField) {
+        throw new Error(`Field ${key} does not exist!`);
+      }
+      fireEvent.change(inputField, { target: { value } });
+    });
+  };
+
+  it('displays errors properly when there are errors in form submission', async () => {
+    const err: IRegistrationError = {
+      username: ['Username cannot be blank'],
+      password: ['Password cannot be blank'],
+      verify_password: ['Verify password cannot be blank'],
+      first_name: ['First name cannot be blank'],
+      last_name: ['Last name cannot be blank'],
+      email: ['Email cannot be blank'],
+      non_field_errors: ['Your form contains invalid inputs'],
+    };
+
+    fillForm();
+
+    (httpClient.auth.register as jest.Mock).mockImplementationOnce(jest.fn(async () => {
+      throw new BadRequestApiError<Partial<IRegistrationError>>('Bad Request Error', err);
+    }));
+
+    await act(async () => {
+      registerButton.click();
+      await tick(1);
+    });
+
+    expect(httpClient.auth.register).toHaveBeenCalledTimes(1);
+
+    (httpClient.auth.register as jest.Mock).mockReset();
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const [message] of Object.values(err)) {
+      // eslint-disable-next-line no-await-in-loop
+      const text = await result.findByText(message);
+      expect(text).toBeTruthy();
+    }
+  });
+
+  it('performs registration when all form values have been filled properly', async () => {
+    fillForm();
+
+    await act(async () => {
+      registerButton.click();
+      await tick(1);
+    });
+
+    // Displays success message
+    expect(httpClient.auth.register).toHaveBeenCalledTimes(1);
+    expect(result.queryByTestId('register_success')).not.toBeEmptyDOMElement();
   });
 });
--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -11,15 +11,103 @@ import {
   Heading,
   Text,
   useColorModeValue,
+  FormErrorMessage,
 } from '@chakra-ui/react';
-import { FC, useState } from 'react';
+import { FC, useState, useCallback } from 'react';
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 import RouterLink from 'components/RouterLink';
 import { useTranslation } from 'react-i18next';
+import { useForm, UseFormRegister } from 'react-hook-form';
+import { IRegistration, IRegistrationError, IUser } from 'customTypes/auth';
+import { useMutation } from 'react-query';
+import { httpClient } from 'api';
+import BadRequestApiError from 'api/error/BadRequestApiError';
+
+const LoginLinkButton: FC = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Stack pt={6}>
+      <Text align="center">
+        {t('already_a_user')}
+        {' '}
+        <RouterLink color="blue.400" to="/auth" data-testid="login-button">{t('login')}</RouterLink>
+      </Text>
+    </Stack>
+  );
+};
+
+interface INameFieldData {
+  firstNameError: string | string[] | undefined;
+  lastNameError: string | string[] | undefined;
+  register: UseFormRegister<IRegistration>;
+}
+
+const NameField: FC<INameFieldData> = ({
+  firstNameError,
+  lastNameError,
+  register,
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <HStack mb={2}>
+      <Box>
+        <FormControl id="firstName" isRequired isInvalid={Boolean(firstNameError)}>
+          <FormLabel htmlFor="firstName">{t('firstName')}</FormLabel>
+          <Input
+            data-testid="first_name"
+            id="firstName"
+            autoComplete="given-name"
+            type="text"
+            {...register('first_name', {
+              required: t('no_blank', { field: t('firstName') })?.[0],
+            })}
+          />
+          <FormErrorMessage>
+            {firstNameError}
+          </FormErrorMessage>
+        </FormControl>
+      </Box>
+      <Box>
+        <FormControl id="lastName" isRequired isInvalid={Boolean(lastNameError)}>
+          <FormLabel htmlFor="lastName">{t('lastName')}</FormLabel>
+          <Input
+            data-testid="last_name"
+            id="lastName"
+            autoComplete="given-name"
+            type="text"
+            {...register('last_name', {
+              required: t('no_blank', { field: t('lastName') })?.[0],
+            })}
+          />
+          <FormErrorMessage>
+            {lastNameError}
+          </FormErrorMessage>
+        </FormControl>
+      </Box>
+    </HStack>
+  );
+};
 
 const RegisterPage: FC = () => {
   const [showPassword, setShowPassword] = useState<boolean>(false);
+  const { register, handleSubmit, formState: { errors: formErrors } } = useForm<IRegistration>();
+  const {
+    mutate, isLoading, error, isSuccess,
+  } = useMutation<
+  IUser,
+  BadRequestApiError<IRegistrationError>,
+  IRegistration
+  >(
+    'register',
+    (values) => httpClient.auth.register(values),
+  );
   const { t } = useTranslation();
+
+  const onSubmit = useCallback((values: IRegistration) => {
+    mutate(values);
+  }, [mutate]);
 
   return (
     <>
@@ -35,59 +123,107 @@ const RegisterPage: FC = () => {
         p={8}
       >
         <Stack spacing={4}>
-          <HStack>
-            <Box>
-              <FormControl id="firstName" isRequired>
-                <FormLabel>{t('firstName')}</FormLabel>
-                <Input type="text" />
+          <form onSubmit={handleSubmit(onSubmit)} data-testid="registerForm">
+            <NameField
+              firstNameError={error?.data?.first_name ?? formErrors?.first_name?.message}
+              lastNameError={error?.data?.last_name ?? formErrors?.last_name?.message}
+              register={register}
+            />
+            <FormControl id="username" isRequired mb={2} isInvalid={Boolean(error?.data?.username ?? formErrors?.username?.message)}>
+              <FormLabel>{t('username')}</FormLabel>
+              <Input
+                type="text"
+                data-testid="username"
+                autoComplete="username"
+                {...register('username', {
+                  required: t('no_blank', { field: t('username') })?.[0],
+                })}
+              />
+              <FormErrorMessage>
+                {error?.data?.username ?? formErrors?.username?.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl id="email" isRequired mb={2} isInvalid={Boolean(error?.data?.email ?? formErrors?.email?.message)}>
+              <FormLabel>{t('email')}</FormLabel>
+              <Input
+                type="email"
+                data-testid="email"
+                autoComplete="email"
+                {...register('email', {
+                  required: t('no_blank', { field: t('email') })?.[0],
+                })}
+              />
+              <FormErrorMessage>
+                {error?.data?.email ?? formErrors?.email?.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl id="new-password" isRequired isInvalid={Boolean(error?.data?.password ?? formErrors?.password?.message)} mb={2}>
+              <FormLabel>{t('password')}</FormLabel>
+              <InputGroup>
+                <Input
+                  type={showPassword ? 'text' : 'password'}
+                  autoComplete="new-password"
+                  data-testid="password"
+                  {...register('password', {
+                    required: t('no_blank', { field: t('password') })?.[0],
+                  })}
+                />
+                <InputRightElement h="full">
+                  <Button
+                    variant="ghost"
+                    data-testid="show-password-button"
+                    onClick={() => setShowPassword((show) => !show)}
+                  >
+                    {showPassword ? <ViewIcon data-testid="password-shown" /> : <ViewOffIcon data-testid="password-hidden" />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+              <FormErrorMessage>
+                {error?.data?.password ?? formErrors?.password?.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl id="verify-password" isRequired isInvalid={Boolean(error?.data?.verify_password ?? formErrors?.verify_password?.message)} mb={2}>
+              <FormLabel>{t('verify_password')}</FormLabel>
+              <InputGroup>
+                <Input
+                  type="password"
+                  autoComplete="new-password"
+                  data-testid="verify_password"
+                  {...register('verify_password', {
+                    required: t('no_blank', { field: t('verify_password') })?.[0],
+                  })}
+                />
+              </InputGroup>
+              <FormErrorMessage>
+                {error?.data?.verify_password ?? formErrors?.verify_password?.message}
+              </FormErrorMessage>
+            </FormControl>
+            <Stack spacing={10} pt={2}>
+              <Button
+                disabled={isLoading || isSuccess}
+                loadingText={t('registering')}
+                type="submit"
+                size="lg"
+                data-testid="registerButton"
+                bg="blue.400"
+                color="white"
+                _hover={{
+                  bg: 'blue.500',
+                }}
+              >
+                {t('register')}
+              </Button>
+              <FormControl id="non-field" isInvalid={Boolean(error?.data?.non_field_errors)} mt={['0 !important']}>
+                <FormErrorMessage>{error?.data?.non_field_errors}</FormErrorMessage>
               </FormControl>
-            </Box>
-            <Box>
-              <FormControl id="lastName">
-                <FormLabel>{t('lastName')}</FormLabel>
-                <Input type="text" />
-              </FormControl>
-            </Box>
-          </HStack>
-          <FormControl id="email" isRequired>
-            <FormLabel>{t('email')}</FormLabel>
-            <Input type="email" />
-          </FormControl>
-          <FormControl id="password" isRequired>
-            <FormLabel>{t('password')}</FormLabel>
-            <InputGroup>
-              <Input type={showPassword ? 'text' : 'password'} data-testid="password" />
-              <InputRightElement h="full">
-                <Button
-                  variant="ghost"
-                  data-testid="show-password-button"
-                  onClick={() => setShowPassword((show) => !show)}
-                >
-                  {showPassword ? <ViewIcon data-testid="password-shown" /> : <ViewOffIcon data-testid="password-hidden" />}
-                </Button>
-              </InputRightElement>
-            </InputGroup>
-          </FormControl>
-          <Stack spacing={10} pt={2}>
-            <Button
-              loadingText="Submitting"
-              size="lg"
-              bg="blue.400"
-              color="white"
-              _hover={{
-                bg: 'blue.500',
-              }}
-            >
-              {t('register')}
-            </Button>
-          </Stack>
-          <Stack pt={6}>
-            <Text align="center">
-              {t('already_a_user')}
-              {' '}
-              <RouterLink color="blue.400" to="/auth" data-testid="login-button">{t('login')}</RouterLink>
-            </Text>
-          </Stack>
+              {isSuccess && (
+              <Text align="center">
+                {t('register_success')}
+              </Text>
+              )}
+            </Stack>
+            <LoginLinkButton />
+          </form>
         </Stack>
       </Box>
     </>
