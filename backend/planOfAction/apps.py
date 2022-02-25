edb1676ce89b13bb921b35ae1b1422fc90b694e7
acyanbird <2534930703@qq.com>
2022-02-25 20:47:56 +0000

    feat:model of plan of action

---
+++ backend/planOfAction/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PlanofactionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'planOfAction'
---
+++ backend/planOfAction/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django_extensions.db.models import TimeStampedModel
+
+
+class PlanOfAction(TimeStampedModel):
+    """
+    Model of plan of action
+    """
+    #  left fk to pair here
+    discription = models.TextField()
+
+
+class MileStone(TimeStampedModel):
+    """
+    Model for milestone connect to plan of action
+    """
+    discription = models.TextField()
+    dateRecorded = models.DateTimeField()
+    planOfAction = models.ForeignKey(PlanOfAction, on_delete=models.CASCADE, null=True)
+    completed = models.BooleanField()
+
+
+class comment(TimeStampedModel):
+    """
+    Model for comment add on milestone(?)
+    """
+    content = models.TextField()
+    milestone = models.ForeignKey(MileStone, on_delete=models.CASCADE, null=True)
