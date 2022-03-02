034c057c2e578c9a2cca435ea13eb8f9513c2157
michaellimair <michael@eventxtra.com>
2022-03-02 16:11:02 +0000

    refactor: access control test

--- frontend/src/components/AccessControl/AccessControl.test.tsx
+++ frontend/src/components/AccessControl/AccessControl.test.tsx
@@ -1,56 +1,44 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
 import { render } from '@testing-library/react';
 import UserContext from 'context/UserContext';
-import { UserGroup } from 'customTypes/auth';
+import { IUser, UserGroup } from 'customTypes/auth';
 import AccessControl from './AccessControl';
 
-describe('components/AccessControl', () => {
+const createMockAccessControlComponent = (
+  allowedGroups: UserGroup[],
+  user?: Partial<IUser>,
+) => render(
+  <UserContext.Provider
+    value={{
+      user: user as IUser,
+      isLoading: false,
+      isUnauthorized: false,
+      refetch: jest.fn(),
+    }}
+  >
+    <AccessControl allowedGroups={allowedGroups}><div>hello</div></AccessControl>
+  </UserContext.Provider>,
+);
+
+describe('components/AccessControl/AccessControl', () => {
   it('allows people with permission to access a resouce', () => {
-    const result = render(
-      <UserContext.Provider
-        value={{
-          user: { groups: [{ id: 1, name: UserGroup.MENTOR }] } as any,
-          isLoading: false,
-          isUnauthorized: false,
-          refetch: jest.fn(),
-        }}
-      >
-        <AccessControl allowedGroups={[UserGroup.MENTOR]}><div>hello</div></AccessControl>
-      </UserContext.Provider>,
+    const result = createMockAccessControlComponent(
+      [UserGroup.MENTOR],
+      { groups: [{ id: 1, name: UserGroup.MENTOR }] },
     );
 
     expect(result.container.innerHTML).toBe('<div>hello</div>');
   });
 
   it('does not allow people with no permission to access a resource', () => {
-    const result = render(
-      <UserContext.Provider
-        value={{
-          user: { groups: [] } as any,
-          isLoading: false,
-          isUnauthorized: false,
-          refetch: jest.fn(),
-        }}
-      >
-        <AccessControl allowedGroups={[UserGroup.MENTOR]}><div>hello</div></AccessControl>
-      </UserContext.Provider>,
-    );
+    const result = createMockAccessControlComponent([UserGroup.MENTOR], { groups: [] });
 
     expect(result.container.innerHTML).toBe('');
   });
 
   it('prevents users that are not logged in from accessing anything', () => {
-    const result = render(
-      <UserContext.Provider
-        value={{
-          user: undefined,
-          isLoading: false,
-          isUnauthorized: true,
-          refetch: jest.fn(),
-        }}
-      >
-        <AccessControl allowedGroups={[UserGroup.MENTOR]}><div>hello</div></AccessControl>
-      </UserContext.Provider>,
+    const result = createMockAccessControlComponent(
+      [UserGroup.MENTOR],
     );
 
     expect(result.container.innerHTML).toBe('');
