ba28b11144177d49e658792e1ccbc5d4c30fe907
SabitRaiyan <sabitraiyan@gmail.com>
2022-03-11 20:44:35 +0000

    feat: implemented add and delete buttons for user skills

--- frontend/src/pages/identity/UserProfile.tsx
+++ frontend/src/pages/identity/UserProfile.tsx
@@ -10,10 +10,11 @@ import {
   FormErrorMessage,
   FormControl,
   InputGroup,
+  Select,
 } from '@chakra-ui/react';
 
 import {
-  FC, useCallback, useEffect, useRef,
+  FC, useCallback, useEffect, useRef, useState,
 } from 'react';
 import { useUser } from 'hooks/useUser';
 import {
@@ -38,7 +39,9 @@ const UserProfile: FC = () => {
   const titleOptions = useTitleOptions();
   const timezoneOptions = useTimezoneOptions();
   const businessAreaOptions = useBusinessAreaOptions();
-  const skillsOptions = useSkillsOptions();
+  const skillsOptions = useSkillsOptions()!;
+  const [userSkillList, setSkillList] = useState(user?.profile.skills!);
+  const [addedArea, setAddedArea] = useState('-');
 
   const { t } = useTranslation();
 
@@ -72,6 +75,19 @@ const UserProfile: FC = () => {
   }, [userProfile, reset]);
 
   const avatarFile = watch('avatar');
+
+  const deleteSkillHandler = (item:any) => {
+    const filtered = userSkillList.filter((value: any) => value !== item);
+    setSkillList(filtered);
+  };
+
+  const addSkillHandler = () => {
+    if (userSkillList.includes(addedArea) || addedArea === '') {
+      return;
+    }
+    const newList = userSkillList.concat([addedArea]);
+    setSkillList(newList);
+  };
 
   return (
     <form id="frm-profile" onSubmit={onSubmit}>
@@ -178,13 +194,33 @@ const UserProfile: FC = () => {
             type="number"
           />
 
-          <FormSelectField
-            name="skills"
-            label={t('skills')}
-            error={errors?.title}
-            register={register}
-            options={skillsOptions}
-          />
+          <FormLabel>Current skills</FormLabel>
+          <Stack spacing={2}>
+            {userSkillList
+              .map((item) => (
+                <Stack direction="row" spacing={4}>
+                  <li key={item} value={item}>{item}</li>
+                  <Button onClick={() => deleteSkillHandler(item)} size="sm" colorScheme="red">Delete</Button>
+                </Stack>
+              ))}
+          </Stack>
+          <FormLabel>Add skills from list below</FormLabel>
+
+          <Stack direction="row">
+            <Select
+              onChange={(e) => {
+                const val = e.target.value;
+                setAddedArea(val);
+              }}
+              value={addedArea}
+            >
+              {skillsOptions.map((item) => (
+                <option key={item.value} value={item.label}>{item.value}</option>
+              ))}
+            </Select>
+            <Button onClick={() => addSkillHandler()} size="sm" colorScheme="green">Add</Button>
+          </Stack>
+
           <FormControl id="non-field" isInvalid={Boolean(errors?.non_field_errors)} mt={['0 !important']}>
             <FormErrorMessage>{errors?.non_field_errors}</FormErrorMessage>
           </FormControl>
