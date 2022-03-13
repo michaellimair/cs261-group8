8962a0414f3dc95f0acd51d793e56a5e6c21a81f
Michael Lim <michaellimbong@gmail.com>
2022-03-13 23:27:45 +0000

    feat: allow for time overlap

--- backend/event/utils.py
+++ backend/event/utils.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from datetime import datetime
 from .models import Event
-from django.db.models import Q
+from django.db.models import Q, F
 
 def has_clashing_events(user: User, start_time: datetime, end_time: datetime) -> bool:
     # Do not allow any overlap between events
@@ -9,7 +9,7 @@ def has_clashing_events(user: User, start_time: datetime, end_time: datetime) ->
         Q(hosts__id__contains=user.id) |
         Q(attendees__id__contains=user.id)
     ).filter(
-        (Q(start_time__range=(start_time, end_time))
-        | Q(end_time__range=(start_time, end_time))
-        | Q(start_time__lt=start_time, end_time__gt=end_time))
+        Q(start_time__gt=F('end_time'), start_time__lt=end_time) |
+        Q(start_time__gt=F('end_time'), end_time__gt=start_time) |
+        Q(start_time__lt=end_time, end_time__gt=start_time)
     ).exists()
